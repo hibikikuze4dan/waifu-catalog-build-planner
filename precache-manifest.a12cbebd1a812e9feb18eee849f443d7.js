8be07cde07339ccd7d6bbd9d45330c33d5a13cfe
Hibiki Kuze <desu2dan@gmail.com>
2020-10-07 19:49:01 -0500

    Updates

---
+++ precache-manifest.a12cbebd1a812e9feb18eee849f443d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a9cb65d4be07856bf2ab6db41b9140",
+    "url": "/waifu-catalog-build-planner/index.html"
+  },
+  {
+    "revision": "a5c98e534c1df2266185",
+    "url": "/waifu-catalog-build-planner/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "afa9d2ebee6281b402bb",
+    "url": "/waifu-catalog-build-planner/static/js/2.1a017a4a.chunk.js"
+  },
+  {
+    "revision": "c37b1bc9635b80e4a2e78366909bab16",
+    "url": "/waifu-catalog-build-planner/static/js/2.1a017a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c98e534c1df2266185",
+    "url": "/waifu-catalog-build-planner/static/js/main.8e675548.chunk.js"
+  },
+  {
+    "revision": "d7bf8adc8fecc0d03f3c",
+    "url": "/waifu-catalog-build-planner/static/js/runtime-main.f9a4c4d8.js"
+  }
+]);
